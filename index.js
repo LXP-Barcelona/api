7d8c2f52245e4625df7608175bc281bdf2d2df78
ForgeOff <86209935+ForgeOfficial@users.noreply.github.com>
2023-10-09 14:33:46 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const database = require('./database.json');
+
+const app = express();
+app.use(express.json());
+
+app.get('/api/products', (req, res) => {
+    const langParam = req.query['lang'] || "en";
+    const result = database.map(item => ({
+        ...item,
+        name: item.name[langParam] || item.name.en,
+        description: item.description[langParam] || item.description.en,
+        specification: item.specification[langParam] || item.specification.en,
+        category: item.category[langParam] || item.category.en,
+    }));
+    res.send(result);
+});
+
+app.get('/api/product/:id', (req, res) => {
+    const id = req.params["id"];
+    const langParam = req.query['lang'] || "en";
+    if (!id) return res.send("error id");
+    try {
+        let item = database.find(item => item.id === parseInt(id));
+        if (!item) return res.send("error item");
+        item = {
+            ...item,
+            name: item.name[langParam] || item.name.en,
+            description: item.description[langParam] || item.description.en,
+            specification: item.specification[langParam] || item.specification.en,
+            category: item.category[langParam] || item.category.en,
+        };
+        res.send(item);
+    } catch (_) {
+        console.log(_);
+        res.send('id is not number')
+    }
+    
+})
+
+app.listen(3000, () => {
+    console.log(`Serveur démarré sur le port 3000`);
+});
