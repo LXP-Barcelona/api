6bb1f9a2807eb5d26500c60a2a7f650658b6a56c
ForgeOff <86209935+ForgeOfficial@users.noreply.github.com>
2023-10-09 15:00:28 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const app = require('express')();
+const express = require('express');
 const database = require('./database.json');
 
+const app = express();
 app.use(express.json());
 
 app.get('/api/products', (req, res) => {
