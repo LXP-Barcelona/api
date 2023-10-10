3de2880bad572ce9e50e330a92a8c5b4295cf193
Festere <cmpnn.romain@gmail.com>
2023-10-10 09:04:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const database = require('./database.json');
+const cors = require('cors');
 
 const app = express();
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 
 app.get('/api/products', (req, res) => {
