aa482c63118caf20d97ef7eaf554b28020cfdb2e
ForgeOff <86209935+ForgeOfficial@users.noreply.github.com>
2023-10-11 17:56:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,22 +25,27 @@ app.get('/api/product/:id', (req, res) => {
     const id = req.params["id"];
     const langParam = req.query['lang'] || "en";
     if (!id) return res.send("error id");
-    try {
-        let item = database.find(item => item.id === parseInt(id));
-        if (!item) return res.send("error item");
-        item = {
-            ...item,
-            name: item.name[langParam] || item.name.en,
-            description: item.description[langParam] || item.description.en,
-            specification: item.specification[langParam] || item.specification.en,
-            category: item.category[langParam] || item.category.en,
-        };
-        res.send(item);
-    } catch (_) {
-        console.log(_);
-        res.send('id is not number')
+    const result = [];
+    const ids = id.includes(",") ? id.split(/,/g) : [id];
+
+    for (let i = 0; i < ids.length; i++) {
+        try {
+            let item = database.find(item => item.id === parseInt(ids[i]));
+            if (!item) return res.send("error item");
+            item = {
+                ...item,
+                name: item.name[langParam] || item.name.en,
+                description: item.description[langParam] || item.description.en,
+                specification: item.specification[langParam] || item.specification.en,
+                category: item.category[langParam] || item.category.en,
+            };
+            result.push(item);
+        } catch (_) {
+            console.log(_);
+        }
     }
-    
+
+    res.send(result);
 })
 
 app.listen(8080, () => console.log(`Server is running in port 8080`));
